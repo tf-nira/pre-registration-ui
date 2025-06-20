fd1de56e0b0074d54cdfea0027b5a32bf57023be
Jagadeesh T <jagadeesh.t@technoforte.co.in>
2025-06-20 12:12:33 +0530

    namefields updated

--- pre-registration-ui/src/app/feature/summary/acknowledgement/acknowledgement.component.ts
+++ pre-registration-ui/src/app/feature/summary/acknowledgement/acknowledgement.component.ts
@@ -183,7 +183,7 @@ export class AcknowledgementComponent implements OnInit, OnDestroy {
             if (!this.userService) {
               this.userService = demographicData["userService"];
             }
-            if(this.userService==appConstants.USER_SERVICE.UPDATE || this.userService==appConstants.USER_SERVICE.FIRSTID|| this.userService==appConstants.USER_SERVICE.REPLACEMENT){
+            if(this.userService==appConstants.USER_SERVICE.UPDATE || this.userService==appConstants.USER_SERVICE.FIRSTID|| this.userService==appConstants.USER_SERVICE.REPLACEMENT || this.userService==appConstants.USER_SERVICE.RENEWAL){
               this.name = appConstants.PRE_REGISTRATION_IDENTITY_NAME_COP;
             }
 
