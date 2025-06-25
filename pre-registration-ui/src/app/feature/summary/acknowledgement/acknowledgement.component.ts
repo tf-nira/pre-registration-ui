e7cec3c828b6858d1f64ba28c070f4f096e58265
Shiba <karthik.s@technoforte.co.in>
2025-06-25 15:10:26 +0530

    prn fro chng in gender

--- pre-registration-ui/src/app/feature/summary/acknowledgement/acknowledgement.component.ts
+++ pre-registration-ui/src/app/feature/summary/acknowledgement/acknowledgement.component.ts
@@ -674,7 +674,7 @@ export class AcknowledgementComponent implements OnInit, OnDestroy {
         ){
           
           if (age >= 16) {
-            if ((demographicData.removingName === "Y" || demographicData.addingName === "Y" || demographicData.completeChangeofName === "Y" || demographicData.changeOfDateOfBirth === "Y" || demographicData.changeInPlaceOfResidence === "Y" || demographicData.addingNamesFromPreviousCertorDoc === "Y")) {
+            if ((demographicData.removingName === "Y" || demographicData.addingName === "Y" || demographicData.completeChangeofName === "Y" || demographicData.changeOfDateOfBirth === "Y" || demographicData.changeInPlaceOfResidence === "Y" || demographicData.addingNamesFromPreviousCertorDoc === "Y" || demographicData.changeInGender === "Y")) {
               const payableServiceCOP:string=this.configService.getConfigByKey("nira.payable.servicecode.COP_NORMAL");
               this.requestBody = {
                 service: payableServiceCOP,
